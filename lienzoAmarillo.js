0adc94a7369246fc865452772f64b23eb55732ee
fernandoglezt <fernando.glezt@gmail.com>
2017-02-16 14:08:10 -0600

    Update lienzoAmarillo.js

--- lienzoAmarillo.js
+++ lienzoAmarillo.js
@@ -1,4 +1,4 @@
       var miCanvas = document.getElementById("miCanvas2");
-      var contexto = miCanvas.getContext("2d");
+      var contexto = miCanvas2.getContext("2d");
       contexto.fillStyle = "yellow";
       contexto.fillRect(30, 30, 50, 50);
