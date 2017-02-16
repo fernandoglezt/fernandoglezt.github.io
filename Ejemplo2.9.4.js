d9ae80ed2925a80afc6c138d29833cbe6fcde477
fernandoglezt <fernando.glezt@gmail.com>
2017-02-16 13:31:30 -0600

    Create Ejemplo2.9.4.js

---
+++ Ejemplo2.9.4.js
@@ -0,0 +1,4 @@
+      var miCanvas = document.getElementById("miCanvas");
+      var contexto = miCanvas.getContext("2d");
+      contexto.fillStyle = "red";
+      contexto.fillRect(30, 30, 50, 50);
