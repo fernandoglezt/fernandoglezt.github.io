0d342c8a8a599cfaafceb311035b4f8e60828995
fernandoglezt <fernando.glezt@gmail.com>
2017-02-21 13:33:53 -0600

    Create Ejemplo1-three.js

---
+++ Ejemplo1-three.js
@@ -0,0 +1,13 @@
+var escena = new THREE.Scene();
+var camara = new THREE.PerspectiveCamera();
+camara.position.z = 5;
+var renderer = new THREE.WebGLRenderer();
+renderer.setSize( window.innerHeight*.9, window.innerHeight*.9 );
+document.body.appendChild( renderer.domElement );
+renderer.render( escena, camara );
+var forma = new THREE.BoxGeometry( 1, 1, 1 );
+var cubo  = new THREE.Mesh( forma );
+cubo.rotation.x += 0.1;
+cubo.rotation.y += 0.1;
+escena.add( cubo );
+renderer.render( escena, camara );
