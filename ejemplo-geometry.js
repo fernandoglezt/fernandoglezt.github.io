e4c5430a7c07475474226a7e0fe9731b2845f8b9
fernandoglezt <fernando.glezt@gmail.com>
2017-02-21 14:10:40 -0600

    Create ejemplo-geometry.js

---
+++ ejemplo-geometry.js
@@ -0,0 +1,34 @@
+var forma = new THREE.Geometry();
+
+forma.vertices.push( new THREE.Vector3( 1,  0,  1 ) ); // Vértice 0
+forma.vertices.push( new THREE.Vector3( 1,  0, -1 ) ); // Vértice 1
+forma.vertices.push( new THREE.Vector3(-1,  0, -1 ) ); // Vértice 2
+forma.vertices.push( new THREE.Vector3(-1,  0,  1 ) ); // Vértice 3
+forma.vertices.push( new THREE.Vector3( 0,  1,  0 ) ); // Vértice 4
+
+forma.faces.push( new THREE.Face3( 3, 2, 1 ) ); // Cara 0
+forma.faces.push( new THREE.Face3( 3, 1, 0 ) ); // Cara 1
+forma.faces.push( new THREE.Face3( 3, 0, 4 ) ); // Cara 2
+forma.faces.push( new THREE.Face3( 0, 1, 4 ) ); // Cara 3
+forma.faces.push( new THREE.Face3( 1, 2, 4 ) ); // Cara 4
+forma.faces.push( new THREE.Face3( 2, 3, 4 ) ); // Cara 5
+
+forma.computeBoundingSphere();
+forma.computeFaceNormals();
+
+var material = new THREE.MeshNormalMaterial();
+
+var malla = new THREE.Mesh( forma, material );
+malla.rotateX(Math.PI/4);
+
+var escena = new THREE.Scene();
+escena.add( malla );
+
+var camara = new THREE.PerspectiveCamera();
+camara.position.z = 5;
+
+var renderizador = new THREE.WebGLRenderer();
+renderizador.setSize( window.innerHeight*.95, 
+                      window.innerHeight*.95 );
+document.body.appendChild( renderizador.domElement );
+renderizador.render( escena, camara );
