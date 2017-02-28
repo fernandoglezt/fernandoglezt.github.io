9311c354764e084e63fba76eab9018f2949c953c
fernandoglezt <fernando.glezt@gmail.com>
2017-02-28 13:31:43 -0600

    Create union-mallas.js

---
+++ union-mallas.js
@@ -0,0 +1,24 @@
+var troncoForma = new THREE.CylinderGeometry(.25, .5, 1);
+var esferaForma = new THREE.SphereGeometry(.65);
+esferaForma.translate(0,1,0);
+
+var troncoMalla = new THREE.Mesh(troncoForma);
+var esferaMalla = new THREE.Mesh(esferaForma);
+
+var arbolForma = new THREE.Geometry();
+arbolForma.merge(troncoMalla.geometry, troncoMalla.matrix);
+arbolForma.merge(esferaMalla.geometry, esferaMalla.matrix);
+
+var material = new THREE.MeshNormalMaterial();
+var arbolMalla = new THREE.Mesh(arbolForma, material);
+
+var escena = new THREE.Scene();
+escena.add(arbolMalla);
+
+var camara = new THREE.PerspectiveCamera();
+camara.position.z = 5;
+
+renderizador = new THREE.WebGLRenderer();
+renderizador.setSize( window.innerHeight*.95, window.innerHeight*.95 );
+document.body.appendChild( renderizador.domElement );
+renderizador.render( escena, camara );
