65c4510cdf6e3d021c8f41d3c9f45abba3a84cac
fernandoglezt <fernando.glezt@gmail.com>
2017-06-11 10:25:10 -0500

    Create pruebasProyecto.js

--- pruebasProyecto.js
+++ pruebasProyecto.js
@@ -28,7 +28,7 @@ function setup(){
   
   
         //Fondo
-	var path = 'http://fernandoglezt.github.io/';
+	var path = '';
         var sides = [ path + 'sbox_px.jpg', path + 'sbox_nx.jpg', path + 'sbox_py.jpg', path + 'sbox_ny.jpg', path + 'sbox_pz.jpg', path + 'sbox_nz.jpg' ];
         var scCube = new THREE.CubeTextureLoader().load(sides);
 	
