91b5e05d03bf899e3f422073ab6dcefde696e3b3
fernandoglezt <fernando.glezt@gmail.com>
2017-06-11 09:50:47 -0500

    Create pruebasProyecto.js

--- pruebasProyecto.js
+++ pruebasProyecto.js
@@ -32,7 +32,8 @@ function setup(){
         var sides = [ path + 'sbox_px.jpg', path + 'sbox_nx.jpg', path + 'sbox_py.jpg', path + 'sbox_ny.jpg', path + 'sbox_pz.jpg', path + 'sbox_nz.jpg' ];
 
         // load images
-        var scCube = THREE.ImageUtils.loadTextureCube(sides);
+        var scCube = THREE.CubeTextureLoader(sides);
+	
         scCube.format = THREE.RGBFormat;
 
         // prepare skybox material (shader)
