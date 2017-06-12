750b047c855688a1100656eb0b9235e8576ba031
fernandoglezt <fernando.glezt@gmail.com>
2017-06-12 01:53:30 -0500

    Create pruebasProyecto.js

--- pruebasProyecto.js
+++ pruebasProyecto.js
@@ -1,10 +1,61 @@
 var malla, camara, renderer, escena, pointLight;
-var pos = 0, posLuna = 0;
+var pos = 0, posLuna = 0, posPersonajeCinco = 0;
 var planetaTierra, luna;
+var personajeCincoM;
 
 
 setup();
 loop();
+
+function personaje5(x, y, z){
+	var esferaForma = new THREE.SphereGeometry( 6*1 );
+	var conoUno = new THREE.ConeGeometry( 6*1, 6*4);
+	var conoDos = new THREE.ConeGeometry( 6*1, 6*4);
+	var conoTres = new THREE.ConeGeometry( 6*1, 6*4);
+	var conoCuatro = new THREE.ConeGeometry( 6*1, 6*4);
+	var conoCinco = new THREE.ConeGeometry( 6*1, 6*4);
+	var conoSeis = new THREE.ConeGeometry( 6*1, 6*4);
+
+	conoDos.rotateZ(Math.PI);
+	conoTres.rotateZ(3*Math.PI/2);
+	conoCuatro.rotateZ(Math.PI/2);
+	conoCinco.rotateX(Math.PI/2);
+	conoSeis.rotateX(3*Math.PI/2);
+
+	conoUno.translate(0, 6*2.8, 0);
+	conoDos.translate(0, 6*-2.8, 0);
+	conoTres.translate(6*2.8, 0, 0);
+	conoCuatro.translate(6*-2.8, 0, 0);
+	conoCinco.translate(0, 0, 6*2.8);
+	conoSeis.translate(0, 0, 6*-2.8);
+
+	var esferaMalla = new THREE.Mesh(esferaForma);
+	var conoMalla1 = new THREE.Mesh(conoUno);
+	var conoMalla2 = new THREE.Mesh(conoDos);
+	var conoMalla3 = new THREE.Mesh(conoTres);
+	var conoMalla4 = new THREE.Mesh(conoCuatro);
+	var conoMalla5 = new THREE.Mesh(conoCinco);
+	var conoMalla6 = new THREE.Mesh(conoSeis);
+
+	var personajeCinco = new THREE.Geometry();
+
+	personajeCinco.merge(esferaMalla.geometry, esferaMalla.matrix);
+	personajeCinco.merge(conoMalla1.geometry, conoMalla1.matrix);
+	personajeCinco.merge(conoMalla2.geometry, conoMalla2.matrix);
+	personajeCinco.merge(conoMalla3.geometry, conoMalla3.matrix);
+	personajeCinco.merge(conoMalla4.geometry, conoMalla4.matrix);
+	personajeCinco.merge(conoMalla5.geometry, conoMalla5.matrix);
+	personajeCinco.merge(conoMalla6.geometry, conoMalla6.matrix);
+
+
+	var material = new THREE.MeshNormalMaterial();
+	personajeCincoM = new THREE.Mesh(personajeCinco, material);
+
+	
+	personajeCinco.translate(x, y, z);
+	escena.add(personajeCincoM);
+
+}
 
 
 function windowResize() {
@@ -32,11 +83,14 @@ function push(e) {
 function actualiza(){
 	planetaTierra.position.x = 600*Math.cos(pos);
 	planetaTierra.position.y = 600*Math.sin(pos);
+	planetaTierra.rotation.y += 0.01;
 	
 	luna.position.x = 600*Math.cos(pos)+120*Math.cos(posLuna);
 	luna.position.y = 600*Math.sin(pos)+120*Math.sin(posLuna);	
 	
-	planetaTierra.rotation.y += 0.01;
+	personajeCincoM.position.x = 90*Math.cos(posPersonajeCinco );
+	personajeCincoM.position.z = 90*Math.sin(posPersonajeCinco );
+	personajeCincoM.rotation.y += 0.05;
 	
 	pos += 0.01;
 	if( pos >= 2*Math.PI)
@@ -44,6 +98,9 @@ function actualiza(){
 	posLuna += 0.05;
 	if( posLuna >= 2*Math.PI)
 		posLuna = 0;
+	posPersonajeCinco += 0.05;
+	if( posPersonajeCinco >= 2*Math.PI)
+		posPersonajeCinco = 0;
 
 }
 
@@ -144,6 +201,8 @@ function setup(){
 			luna.position.y = 400+120;
 			escena.add(luna);
 	} );
+	
+	personaje5(0, 30, 0);
 }
 
 
